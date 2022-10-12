e935adeffc1f1110e842b9be191753f461aafa83
PG <szadowek91@gmail.com>
2022-10-12 12:43:14 +0200

    Initial commit

---
+++ src/main/java/pl/szadowek91/remoteJobs/RemoteJobsApplication.java
@@ -0,0 +1,13 @@
+package pl.szadowek91.remoteJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szadowek91/remoteJobs/RemoteJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szadowek91.remoteJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
